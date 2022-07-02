50605dffd625cce0d7bc4bc5f7ef664829328871
Qianpeng Li <55045729+QianpengLi577@users.noreply.github.com>
2022-07-02 16:15:28 +0800

    add 0702

--- test_0702/evaluation/test_nothing.py
+++ test_0702/evaluation/test_nothing.py
@@ -18,9 +18,13 @@ class SCNN(nn.Module):
         h1_mem = h1_spike = h1_sumspike = torch.zeros(batch_size, cfg_fc[0], device=device)
         h2_mem = h2_spike = h2_sumspike = torch.zeros(batch_size, cfg_fc[1], device=device)
         count_spike = 0
-        # 计算为了查找相应的神经元需要计算的次数，第一是计算1bit相加的次数，第二个是计算nbit相加的次数
+        # 路由方面的查找次数是一致的，因此不额外计算
+        # 计算为了查找相应的神经元需要计算的次数，第一是计算1bit相加的次数，第二个是计算nbit相加的次数  这个对应的是体系结构7.1-1
         count_add_1 = 0 # direct index 
         count_add_2 = 0 # step index
+
+        count_add_3 = 0 # direct index           7.1-2
+        count_add_4 = 0 # two h-direct index     7.1-2
         for step in range(time_window): # simulation time steps
             x = input > torch.rand(input.size(), device=device) # prob. firing
             x = x.view(batch_size, -1)
@@ -53,6 +57,14 @@ class SCNN(nn.Module):
                                 count_add_1+= np.nonzero(temp)[0][len(np.nonzero(temp)[0])-1]-1
                                 count_add_2+= core_mem_all[k][index_l_[j]+784][1]
 
+                # event = np.zeros(1024)
+                # event[0:784] = x_
+                # event[784:912] = l_
+                # for i in range(core_num):
+                #     for j in range(core_size):
+                #         bitmap = core_index_all_0702[i][j][0]
+                #         step_indexx =  core_index_all_0702[i][j][1]
+                        
 
         outputs = h2_sumspike / time_window
         # print('add_direct:',count_add_1)
@@ -83,10 +95,15 @@ weight_all[index2]=0
 p=np.count_nonzero(index)/(784*128+128*10)
 print('sparsity',p)
 
+index_temp = index.T
+index = ((index+index.T)>=1).astype(np.int32)
+
 snn.fc1.weight.data= torch.from_numpy(weight_all[0:784,784:912].T).float()
 snn.fc2.weight.data= torch.from_numpy(weight_all[784:912,912:922].T).float()
 
 
+# core_index 表示每个核里对应的真实神经元id core_num*core_size
+# core_mem_all有5个mem，每个mem有922个[bitmap，num],bitmap长度为coresize
 
 shuffle_ix = np.random.permutation(np.arange(core_num*core_size))
 core_index = shuffle_ix.reshape((core_num,core_size))
@@ -108,6 +125,26 @@ for i in range(core_num):
         num = np.count_nonzero(bitmap, axis=None)
         mem.append([bitmap,num])
     core_mem_all.append(mem)
+
+N_drc = 1024
+len_step_index=8
+core_index_all_0702=[]
+for i in range(core_num):
+    core_ = core_index[i,:]
+    mem = []
+    for j in range(core_size):
+        bitmap = np.zeros(N_drc)
+        step_index = np.zeros(int(len_step_index))
+        for m in range(922):
+            if (core_[j]<922):
+                bitmap[m]=index[m,core_[j]]
+        for n in range(len_step_index):
+            step_index[n]=np.sum(bitmap[0:(n+1)*int(N_drc/len_step_index)])
+        # num = np.count_nonzero(bitmap, axis=None)
+        mem.append([bitmap,step_index])
+    core_index_all_0702.append(mem)
+
+
 
 names = 'two_fc_0627_'
 data_path =  './raw/' #todo: input your data path
@@ -162,6 +199,7 @@ acc = 100. * float(correct) / float(total)
 print('Test Acc: %.5f' % acc)
 print('Time: %.5f' % (end_time - begin_time))
 
+print('th',th)
 print('sparsity',p)
 print('add_direct per neuron per neuron per timestep:',c1/20/10000/(912))
 print('add_step per neuron per timestep',c2/20/10000/(912))
